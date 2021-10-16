647e1052f57ed88ca0a38d5c4589d0cba268b321
zuocheng li <zuocheng.li@sjsu.edu>
2021-10-16 11:53:17 +0800

    added ArrayListUtils

---
+++ src/ArrayListUtils.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * Class that provides helper functions for ArrayList
+ */
+public class ArrayListUtils {
+
+    /**
+     * Calculates total sum of all elements in the given array
+     *
+     * @param arr array list of integers for which we calculate sum
+     * @return the total sum of all elements in the array
+     */
+    public static int sum(ArrayList<Integer> arr) {
+        // TODO: implement this method on a separate branch. Should return sum of all elements in the array list
+        return 0;
+    }
+
+    /**
+     * Calculates average value of all elements in the given array
+     *
+     * @param arr array list of integers for which we calculate average
+     * @return average value of all elements in the array
+     */
+    public static int mean(ArrayList<Integer> arr) {
+        // TODO: implement this method on a separate branch. Should return the average of elements in the array list
+        return 0;
+    }
+}
